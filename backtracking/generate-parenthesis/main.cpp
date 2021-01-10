1c0bcea7a712a82c55ddcd1918017f04b2ed3448
wangzhenghua01 <wangzhenghua01@baidu.com>
2021-01-10 18:22:38 +0800

    generate parenthesis

---
+++ backtracking/generate-parenthesis/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> generate_parenthesis(int n) {
+        vector<string> result;
+        if(n <= 0)
+            return result;
+        helper(n, n, "", result);
+        return result;
+    }
+private:
+    void helper(int left, int right, string tmp, vector<string>& res) {
+        if(left < 0 || right < 0 || left > right) return;
+        if(left == 0 && right == 0) {
+            res.push_back(tmp);
+            return;
+        }
+        helper(left-1, right, tmp+"(", res);
+        helper(left, right-1, tmp+")", res);
+    }
+};
+
+int main() {
+    int n = 3;
+    vector<string> parenthesis = Solution().generate_parenthesis(n);
+    for(int i = 0; i < parenthesis.size(); i++) {
+        cout << parenthesis[i] << endl;
+    }
+}
---
+++ backtracking/generate-parenthesis/main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+#encoding utf-8
+
+class Solution(object):
+    def generateParenthesis(self, n):
+        result = []
+        if(n <= 0):
+            return result
+        self.helper(n, n, "", result)
+        return result
+
+    def helper(self, left, right, tmp, result):
+        if(left < 0 or right < 0 or left > right):
+            return
+        if(left == 0 and right == 0):
+            result.append(tmp)
+        self.helper(left - 1, right, tmp + "(", result)
+        self.helper(left, right - 1, tmp + ")", result)
+
+if __name__ == '__main__':
+    n = 3
+    result = Solution().generateParenthesis(n)
+    for item in result:
+        print(item)
